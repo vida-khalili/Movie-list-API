92bf62e4604ac6bfbcab982945e9711606f15a21
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2023-07-15 19:31:44 +0330

    Updated script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const apiKey = "b8ace792";
+let movieDetails;
+
+let fetchAPI = async () => {
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=mother`
+    );
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const getMovieDetails = async () => {
+  movieDetails = await fetchAPI();
+};
